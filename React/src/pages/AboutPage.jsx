b5a2cf4aec640500044d3f1625a672009c358a93
vijethnarayan-24 <vijeth.thedreamerdesigns@gmail.com>
2026-08-22 19:06:05 +0530

    feat: replace stats counter section in About page with Clinical Trust & Accreditation section

--- React/src/pages/AboutPage.jsx
+++ React/src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import Breadcrumb from '../components/common/Breadcrumb';
 import WelcomeArea from '../components/home/WelcomeArea';
+import ClinicalTrustStrip from '../components/home/ClinicalTrustStrip';
 import WhyChooseUs from '../components/home/WhyChooseUs';
 import Testimonials from '../components/home/Testimonials';
 import { aboutContent } from '../content/aboutContent';
@@ -13,25 +14,7 @@ const AboutPage = () => {
     <MainLayout>
       <Breadcrumb title={content.header.title} activeTitle={content.header.activeTitle} />
       <WelcomeArea />
-      
-      {/* Start fact counter area */}
-      <section className="fact-counter-area" style={{ background: '#131d33', padding: '60px 0', color: '#fff' }}>
-        <div className="container">
-          <div className="row text-center">
-            {content.stats.map(stat => (
-              <div key={stat.id} className="col-md-3 col-sm-6 col-xs-12">
-                <div className="single-fact-counter" style={{ padding: '20px' }}>
-                  <span className={stat.icon} style={{ fontSize: '40px', color: '#c59d5f' }}></span>
-                  <h1 style={{ fontSize: '42px', fontWeight: 700, margin: '10px 0', color: '#fff' }}>{stat.number}</h1>
-                  <p style={{ color: '#aaa', textTransform: 'uppercase', letterSpacing: '1px' }}>{stat.label}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      {/* End fact counter area */}
-
+      <ClinicalTrustStrip />
       <WhyChooseUs />
       <Testimonials />
 
