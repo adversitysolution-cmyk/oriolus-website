abd60d00ee5985cf3528a14a221ddbe63bde8b3d
vijethnarayan-24 <vijeth.thedreamerdesigns@gmail.com>
2026-08-23 17:15:01 +0530

    fix: restore authentic Daily Spanz template UI, classes, hover effects, and sidebar layout on Procedures page

--- React/src/pages/ProceduresPage.jsx
+++ React/src/pages/ProceduresPage.jsx
@@ -12,17 +12,19 @@ const ProceduresPage = () => {
     <MainLayout>
       <Breadcrumb title={content.header.title} activeTitle={content.header.activeTitle} />
 
-      <section className="procedures-page-area" style={{ padding: '80px 0' }}>
+      {/* Start Procedures Area */}
+      <section className="procedures-area pd-top100 pd-bottom100">
         <div className="container">
           <div className="row">
-            {/* Procedures Grid (8 cols) */}
-            <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+            
+            {/* Start Procedures Grid (9 cols right) */}
+            <div className="col-lg-9 col-md-8 col-sm-12 col-xs-12 pull-right">
               <div className="row">
                 {content.items.map(item => (
-                  <div key={item.id} className="col-md-6 col-sm-6 col-xs-12" style={{ marginBottom: '30px' }}>
-                    <div className="single-item" style={{ background: '#fff', border: '1px solid #eee', overflow: 'hidden' }}>
-                      <div className="img-holder" style={{ position: 'relative', overflow: 'hidden' }}>
-                        <img src={item.image} alt={item.title} style={{ width: '100%', height: '220px', objectFit: 'cover' }} />
+                  <div key={item.id} className="col-md-6 col-sm-6 col-xs-12">
+                    <div className="single-item">
+                      <div className="img-holder">
+                        <img src={item.image} alt={item.title} />
                         <div className="overlay-style-one">
                           <div className="box">
                             <div className="content">
@@ -33,22 +35,19 @@ const ProceduresPage = () => {
                           </div>  
                         </div>    
                       </div>
-                      <div className="text-holder text-center" style={{ padding: '25px 20px' }}>
+                      <div className="text-holder text-center">
                         <div className="show-content">
-                          <div className="iocn-holder" style={{ marginBottom: '10px' }}>
-                            <span className={item.icon} style={{ fontSize: '36px', color: '#c59d5f' }}></span>      
+                          <div className="iocn-holder">
+                            <span className={item.icon}></span>      
                           </div>
-                          <h3 style={{ fontSize: '18px', margin: '0 0 10px', color: '#262626' }}>{item.title}</h3>
+                          <h3>{item.title}</h3>
                         </div>
                         <div className="hover-content">
                           <Link to={item.link}>
-                            <h3 style={{ fontSize: '18px', margin: '0 0 10px', color: '#262626' }}>{item.title}</h3>
+                            <h3>{item.title}</h3>
                           </Link>
-                          <span className="border" style={{ display: 'block', width: '30px', height: '2px', background: '#c59d5f', margin: '0 auto 12px' }}></span>
-                          <p style={{ fontSize: '13px', color: '#777', lineHeight: '1.6', marginBottom: '15px' }}>{item.shortDesc}</p>
-                          <Link to={item.link} className="thm-btn bgclr-1" style={{ padding: '8px 20px', fontSize: '12px' }}>
-                            Read More
-                          </Link>
+                          <span className="border"></span>
+                          <p>{item.shortDesc}</p>
                         </div>
                       </div>
                     </div>
@@ -56,14 +55,16 @@ const ProceduresPage = () => {
                 ))}
               </div>
             </div>
+            {/* End Procedures Grid */}
 
-            {/* Sidebar (4 cols) */}
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <ProcedureSidebar currentPath="/procedures" />
-            </div>
+            {/* Start Sidebar (3 cols left) */}
+            <ProcedureSidebar />
+            {/* End Sidebar */}
+
           </div>
         </div>
       </section>
+      {/* End Procedures Area */}
     </MainLayout>
   );
 };
